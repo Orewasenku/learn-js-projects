bc521ab8a26f2d74293b1459ac05f7add30277e9
Orewa Senku <oorewasenku@gmail.com>
2025-09-19 12:15:23 +0100

    initial commit

---
+++ First Lesson.js
@@ -0,0 +1,15 @@
+alert('hello'); //popup message
+
+alert('You are doing a good job!!'); //Command the wepage to display popup messages
+
+document.body.innerHTML ='Hello';  //Modifying the body of a webpage
+
+2+2*8  //Doing maths
+
+//Assesment:
+alert ('My name is Ologun Folahan Armstrong'); //popup message with my name
+undefined
+10+5 //addition of expencess 
+15 
+document.body.innerHTML ='';
+'' //Makes the body of the webpage empty
